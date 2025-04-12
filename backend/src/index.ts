a51046e45c967a8f1c037620e7416f0738afc88c
AnastasiaR2 <anastasiia.romaniuk007@gmail.com>
2025-04-12 22:41:30 +0300

    Create error handler middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, Application } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { errorHandler } from './middlewares/errorHandler';
 
 dotenv.config();
 
@@ -10,9 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
-});
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
